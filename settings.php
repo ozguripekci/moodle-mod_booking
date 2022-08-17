ce5932a9ccc5ec2da311a72331fbcb393886a6f6
stepanCodam <96595300+stepanCodam@users.noreply.github.com>
2022-08-17 09:43:06 +0200

    changes to settings.php to fix site admin plugins section

--- settings.php
+++ settings.php
@@ -28,6 +28,7 @@ global $CFG, $ADMIN, $DB;
 require_once($CFG->dirroot . '/mod/booking/lib.php');
 require_once($CFG->dirroot . '/user/profile/lib.php');
 
+use mod_booking\price;
 use \mod_booking\utils\wb_payment;
 
 $ADMIN->add('modsettings',
@@ -36,28 +37,47 @@ $ADMIN->add('modsettings',
 
 $ADMIN->add('modbookingfolder', $settings);
 
+$ADMIN->add('modbookingfolder',
+        new admin_externalpage('modbookingoptionformconfig',
+                get_string('optionformconfig', 'mod_booking'),
+                new moodle_url('/mod/booking/optionformconfig.php')));
+
+$ADMIN->add('modbookingfolder',
+        new admin_externalpage('modbookingpricecategories',
+                get_string('pricecategories', 'mod_booking'),
+                new moodle_url('/mod/booking/pricecategories.php')));
+
+$ADMIN->add('modbookingfolder',
+        new admin_externalpage('modbookingsemesters',
+                get_string('booking:semesters', 'mod_booking'),
+                new moodle_url('/mod/booking/semesters.php')));
+
+$ADMIN->add('modbookingfolder',
+        new admin_externalpage('modbookingcustomfield',
+                get_string('customfieldconfigure', 'mod_booking'),
+                new moodle_url('/mod/booking/customfield.php')));
+
 if ($ADMIN->fulltree) {
-    $settings->add(
-        new admin_setting_heading('mod_booking_licensekeycfg',
-            get_string('licensekeycfg', 'mod_booking'),
-            get_string('licensekeycfgdesc', 'mod_booking')));
+        $settings->add(
+        new admin_setting_heading('licensekeycfgheading',
+        get_string('licensekeycfg', 'mod_booking'),
+        get_string('licensekeycfgdesc', 'mod_booking')));
 
-    // Dynamically change the license info text.
-    $licensekeydesc = get_string('licensekeydesc', 'mod_booking');
+        // Dynamically change the license info text.
+        $licensekeydesc = get_string('licensekeydesc', 'mod_booking');
 
-    // get license key which has been set in text field
-    $pluginconfig = get_config('booking');
-    if (!empty($pluginconfig->licensekey)){
-        $licensekey = $pluginconfig->licensekey;
-
-        $expiration_date = wb_payment::decrypt_licensekey($licensekey);
-        if (!empty($expiration_date)){
-            $licensekeydesc = "<p style='color: green; font-weight: bold'>"
+        // Get license key which has been set in text field.
+        $pluginconfig = get_config('booking');
+    if (!empty($pluginconfig->licensekey)) {
+            $licensekey = $pluginconfig->licensekey;
+            $expirationdate = wb_payment::decrypt_licensekey($licensekey);
+        if (!empty($expirationdate)) {
+                $licensekeydesc = "<p style='color: green; font-weight: bold'>"
                 .get_string('license_activated', 'mod_booking')
-                .$expiration_date
+                .$expirationdate
                 .")</p>";
         } else {
-            $licensekeydesc = "<p style='color: red; font-weight: bold'>"
+                $licensekeydesc = "<p style='color: red; font-weight: bold'>"
                 .get_string('license_invalid', 'mod_booking')
                 ."</p>";
         }
@@ -68,6 +88,108 @@ if ($ADMIN->fulltree) {
         new admin_setting_configtext('booking/licensekey',
             get_string('licensekey', 'mod_booking'),
             $licensekeydesc, ''));
+            $settings->add(
+                new admin_setting_heading('bookingpricesettings_heading',
+                    get_string('bookingpricesettings', 'mod_booking'),
+                    get_string('bookingpricesettings_desc', 'mod_booking')));
+        
+            // Choose the user profile field which is used to store each user's price category.
+            $userprofilefields = $DB->get_records('user_info_field', null, '', 'id, name, shortname');
+            if (!empty($userprofilefields)) {
+                $userprofilefieldsarray = [];
+                $userprofilefieldsarray[0] = get_string('pricecategoryfieldoff', 'mod_booking');
+        
+                // Create an array of key => value pairs for the dropdown.
+                foreach ($userprofilefields as $userprofilefield) {
+                    $userprofilefieldsarray[$userprofilefield->shortname] = $userprofilefield->name;
+                }
+        
+                $settings->add(
+                new admin_setting_configselect('booking/pricecategoryfield',
+                    get_string('pricecategoryfield', 'mod_booking'),
+                    get_string('pricecategoryfielddesc', 'mod_booking'),
+                    0, $userprofilefieldsarray));
+            }
+        
+        //     // Currency dropdown.
+        //     $currenciesobjects = price::get_possible_currencies();
+        
+        //     $currencies = [];
+        
+        //     foreach ($currenciesobjects as $currenciesobject) {
+        //         $currencyidentifier = $currenciesobject->get_identifier();
+        //         $currencies[$currencyidentifier] = $currenciesobject->out(current_language()) . ' (' . $currencyidentifier . ')';
+        //     }
+        
+        //     $settings->add(
+        //         new admin_setting_configselect('booking/globalcurrency',
+        //             get_string('globalcurrency', 'booking'),
+        //             get_string('globalcurrencydesc', 'booking'),
+        //             'EUR', $currencies));
+        
+            $settings->add(
+                new admin_setting_configtextarea('booking/defaultpriceformula',
+                    get_string('defaultpriceformula', 'booking'),
+                    get_string('defaultpriceformuladesc', 'booking'), '', PARAM_TEXT, 60, 10));
+        
+            $settings->add(
+                new admin_setting_heading('duplicationrestore',
+                    get_string('duplicationrestore', 'mod_booking'),
+                    get_string('duplicationrestoredesc', 'mod_booking')));
+        
+            $settings->add(
+                new admin_setting_configcheckbox('booking/duplicationrestoreteachers',
+                        get_string('duplicationrestoreteachers', 'mod_booking'), '', 1));
+        
+            $settings->add(
+                new admin_setting_configcheckbox('booking/duplicationrestoreprices',
+                        get_string('duplicationrestoreprices', 'mod_booking'), '', 1));
+        
+            $settings->add(
+                new admin_setting_configcheckbox('booking/duplicationrestoreentities',
+                        get_string('duplicationrestoreentities', 'mod_booking'), '', 1));
+        
+        
+            $sql = "SELECT cff.shortname FROM {customfield_category} cfc LEFT JOIN
+            {customfield_field} cff on cfc.id = cff.categoryid
+            where cfc.component = 'mod_booking'";
+            global $DB;
+        
+            $records = $DB->get_records_sql($sql);
+        
+            foreach ($records as $record) {
+                $options[$record->shortname] = $record->shortname;
+            }
+        
+            if (isset($options)) {
+                $settings->add(
+                    new admin_setting_configselect('booking/newcoursecategorycfield',
+                            get_string('newcoursecategorycfield', 'mod_booking'),
+                            get_string('newcoursecategorycfielddesc', 'mod_booking'),
+                            1, $options));
+            }
+        
+            $settings->add(
+                new admin_setting_heading('notificationlist',
+                    get_string('notificationlist', 'mod_booking'),
+                    get_string('notificationlistdesc', 'mod_booking')));
+        
+            $settings->add(
+                new admin_setting_configcheckbox('booking/usenotificationlist',
+                        get_string('usenotificationlist', 'mod_booking'), '', 0));
+        
+            // TODO.
+            $settings->add(
+                new admin_setting_heading('uniqueoptionnameheading',
+                    get_string('uniqueoptionnameheading', 'mod_booking'),
+                    get_string('uniqueoptionnameheadingdesc', 'mod_booking')));
+        
+            $settings->add(
+                new admin_setting_configtext('booking/uniqueoptionnameseparator',
+                    get_string('uniqueoptionnameseparator', 'mod_booking'),
+                    get_string('uniqueoptionnameseparatordesc', 'mod_booking'),
+                    '#?#', PARAM_RAW_TRIMMED));
+                    // PARAM_RAW_TRIMMED makes sure that there are no leading or trailing whitespaces.        
     $settings->add(
             new admin_setting_heading('mod_booking_icalcfg',
                 get_string('icalcfg', 'mod_booking'),
